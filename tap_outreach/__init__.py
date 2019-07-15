d31b6a85598b1dc0d95b615b3f178afd21e854b0
cmbramwell <bramwell.christian@gmail.com>
2019-07-14 21:17:15 -0700

    update init file

--- tap_outreach/__init__.py
+++ tap_outreach/__init__.py
@@ -7,6 +7,7 @@ import re
 import sys
 import json
 
+#import attr
 import backoff
 import requests
 import singer
@@ -14,9 +15,9 @@ import singer.messages
 import singer.metrics as metrics
 from singer import metadata
 from singer import utils
-from singer import (transform,
-                    UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING,
-                    Transformer, _transform_datetime)
+from singer import Transformer
+from tap_outreach.context import Context
+import tap_outreach.streams # Load stream objects into Context
 
 REQUIRED_CONFIG_KEYS = ["redirect_uri",
                         "client_id",
@@ -55,7 +56,6 @@ ENDPOINTS = {
     "stages":                       "/v2/stages"
 }
 
-
 def acquire_access_token_from_refresh_token():
     payload = {
         "grant_type": "refresh_token",
@@ -78,34 +78,6 @@ def acquire_access_token_from_refresh_token():
         datetime.timedelta(seconds=auth['expires_in'] - 600))
     LOGGER.info("Token refreshed. Expires at %s", CONFIG['token_expires'])
 
-'''
-def get_start(state, tap_stream_id, bookmark_key):
-    current_bookmark = singer.get_bookmark(state, tap_stream_id, bookmark_key)
-    if current_bookmark is None:
-        return CONFIG['start_date']
-    return current_bookmark
-
-
-def has_bookmark(state, tap_stream_id, bookmark_key):
-    return singer.get_bookmark(state, tap_stream_id, bookmark_key) is not None
-
-
-def get_previous_time_window(state, tap_stream_id):
-    return singer.get_bookmark(state, tap_stream_id, "last_sync_duration")
-
-
-def write_stream_duration(state, tap_stream_id, start, end):
-    duration = (end - start).total_seconds()
-    return singer.write_bookmark(state, tap_stream_id, "last_sync_duration", duration)
-
-
-def get_url(endpoint, **kwargs):
-    if endpoint not in ENDPOINTS:
-        raise ValueError("Invalid endpoint {}".format(endpoint))
-
-    return BASE_URL + ENDPOINTS[endpoint].format(**kwargs)
-'''
-
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
@@ -121,23 +93,41 @@ def load_schemas():
 
     return schemas
 
+def get_discovery_metadata(stream, schema):
+    mdata = metadata.new()
+    mdata = metadata.write(mdata, (), 'table-key-properties', stream.key_properties)
+    mdata = metadata.write(mdata, (), 'forced-replication-method', stream.replication_method)
+
+    if stream.replication_key:
+        mdata = metadata.write(mdata, (), 'valid-replication-keys', [stream.replication_key])
+
+    for field_name in schema['properties'].keys():
+        if field_name in stream.key_properties or field_name == stream.replication_key:
+            mdata = metadata.write(mdata, ('properties', field_name), 'inclusion', 'automatic')
+        else:
+            mdata = metadata.write(mdata, ('properties', field_name), 'inclusion', 'available')
+
+    return metadata.to_list(mdata)
+
 def discover():
     raw_schemas = load_schemas()
     streams = []
 
     for schema_name, schema in raw_schemas.items():
+        if schema_name not in Context.stream_objects:
+            continue
 
-        # TODO: populate any metadata and stream's key properties here..
-        stream_metadata = []
-        stream_key_properties = []
+        stream = Context.stream_objects[schema_name]()
 
         # create and add catalog entry
         catalog_entry = {
             'stream': schema_name,
             'tap_stream_id': schema_name,
             'schema': schema,
-            'metadata' : [],
-            'key_properties': []
+            'metadata': get_discovery_metadata(stream, schema),
+            'key_properties': stream.key_properties,
+            'replication_key': stream.replication_key,
+            'replication_method': stream.replication_method
         }
         streams.append(catalog_entry)
 
@@ -158,24 +148,71 @@ def get_selected_streams(catalog):
 
     return selected_streams
 
-def sync(config, state, catalog):
+def sync():
 
-    selected_stream_ids = get_selected_streams(catalog)
+    # Emit all schemas first so we have them for child streams
+    for stream in Context.catalog["streams"]:
+        if Context.is_selected(stream["tap_stream_id"]):
+            singer.write_schema(stream["tap_stream_id"],
+                                stream["schema"],
+                                stream["key_properties"],
+                                bookmark_properties=stream["replication_key"])
+            Context.counts[stream["tap_stream_id"]] = 0
+
+    # If there is a currently syncing stream bookmark, shuffle the
+    # stream order so it gets sync'd first
+    currently_sync_stream_name = Context.state.get('bookmarks', {}).get('currently_sync_stream')
+    if currently_sync_stream_name:
+        shuffle_streams(currently_sync_stream_name)
 
     # Loop over streams in catalog
-    for stream in catalog.streams:
-        stream_id = stream.tap_stream_id
-        stream_schema = stream.schema
-        if stream_id in selected_stream_ids:
-            # TODO: sync code for stream goes here...
-            LOGGER.info('Syncing stream:' + stream_id)
+    for catalog_entry in Context.catalog['streams']:
+        stream_id = catalog_entry['tap_stream_id']
+        stream = Context.stream_objects[stream_id]()
 
-            querystring = {"sort": "-updatedAt",
-                            "page[limit]": "100",
-                            "filter[updatedAt]": start_date + ".." + end_date}
+        if not Context.is_selected(stream_id):
+            LOGGER.info('Skipping stream: %s', stream_id)
+            continue
 
+        LOGGER.info('Syncing stream: %s', stream_id)
 
-    return
+        if not Context.state.get('bookmarks'):
+            Context.state['bookmarks'] = {}
+        Context.state['bookmarks']['currently_sync_stream'] = stream_id
+
+        with Transformer() as transformer:
+            for rec in stream.sync():
+                extraction_time = singer.utils.now()
+                record_schema = catalog_entry['schema']
+                record_metadata = metadata.to_map(catalog_entry['metadata'])
+                rec = transformer.transform(rec, record_schema, record_metadata)
+                singer.write_record(stream_id,
+                                    rec,
+                                    time_extracted=extraction_time)
+                Context.counts[stream_id] += 1
+
+        Context.state['bookmarks'].pop('currently_sync_stream')
+        singer.write_state(Context.state)
+
+    LOGGER.info('----------------------')
+    for stream_id, stream_count in Context.counts.items():
+        LOGGER.info('%s: %d', stream_id, stream_count)
+    LOGGER.info('----------------------')
+
+def giveup(exc):
+    return exc.response is not None \
+        and 400 <= exc.response.status_code < 500 \
+        and exc.response.status_code != 429
+
+def on_giveup(details):
+    if len(details['args']) == 2:
+        url, params = details['args']
+    else:
+        url = details['args']
+        params = {}
+
+    raise Exception("Giving up on request after {} tries with url {} and params {}" \
+                    .format(details['tries'], url, params))
 
 @backoff.on_exception(backoff.constant,
                       (requests.exceptions.RequestException,
@@ -240,35 +277,29 @@ def gen_request(STATE, tap_stream_id, url, params, path, more_key, offset_keys, 
     STATE = singer.clear_offset(STATE, tap_stream_id)
     singer.write_state(STATE)
 
-STREAMS = [
-    # Do these first as they are incremental
-
-    # Do these last as they are full table
-    Stream('prospects', sync_prospects, ['id'], 'updatedAt', 'FULL_TABLE'),
-    Stream('sequences', sync_sequences, ['id'], 'updatedAt', 'FULL_TABLE'),
-]
-
 @utils.handle_top_exception(LOGGER)
 def main():
 
     # Parse command line arguments
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    CONFIG.update(args.config)
-    acquire_access_token_from_refresh_token()
-
     # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
         catalog = discover()
+        print(discover())
         print(json.dumps(catalog, indent=2))
     # Otherwise run in sync mode
     else:
+        Context.tap_start = utils.now()
         if args.catalog:
-            catalog = args.catalog
+            Context.catalog = args.catalog.to_dict()
         else:
-            catalog =  discover()
+            Context.catalog = discover()
 
-        sync(CONFIG, args.state, catalog)
+        Context.config = args.config
+        Context.state = args.state
+
+        sync()
 
 if __name__ == "__main__":
     main()
